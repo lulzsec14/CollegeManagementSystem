1feb5e6ad84983bfe81d4123dbb768ff4c63bf64
Aryan Gupta <aryan678gupta@gmail.com>
2021-11-01 06:08:19 +0530

    export fixed

--- server/controllers/Requests/requestController.js
+++ server/controllers/Requests/requestController.js
@@ -1,6 +1,6 @@
 const Requests = require('../../models/Requests');
 const factory = require('../DAO/dao');
 
-exports.getPhoto = factory.getOne(Requests);
-exports.updatePhoto = factory.updateOne(Requests);
-exports.deletePhoto = factory.deleteOne(Requests);
+exports.getRequest = factory.getOne(Requests);
+exports.updateRequest = factory.updateOne(Requests);
+exports.deleteRequest = factory.deleteOne(Requests);
